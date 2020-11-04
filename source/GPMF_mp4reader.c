5a39daf073d6630d21f26c5385ca8d36526fbb93
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-11-04 15:44:42 -0800

    Rename GPMF_mp4reader.c to source/GPMF_mp4reader.c

--- source/GPMF_mp4reader.c
+++ source/GPMF_mp4reader.c
@@ -201,7 +201,7 @@ size_t OpenMP4Source(char *filename, uint32_t traktype, uint32_t traksubtype)  /
 					break;
 				}
 
-				if (!GPMF_VALID_FOURCC(qttag) && (qttag & 0xff) != 0xa9) // ©xyz and ©swr are allowed
+				if (!GPMF_VALID_FOURCC(qttag) && (qttag & 0xff) != 0xa9) // Â©xyz and Â©swr are allowed
 				{
 					CloseSource((size_t)mp4);
 					mp4 = NULL;
@@ -361,8 +361,8 @@ size_t OpenMP4Source(char *filename, uint32_t traktype, uint32_t traksubtype)  /
 								readnum = BYTESWAP32(readnum);
 								if (readnum <= (qtsize / 12) && mp4->trak_clockdemon)
 								{
-									uint32_t segment_duration; //integer that specifies the duration of this edit segment in units of the movie’s time scale.
-									uint32_t segment_mediaTime; //integer containing the starting time within the media of this edit segment(in media timescale units).If this field is set to ?, it is an empty edit.The last edit in a track should never be an empty edit.Any difference between the movie’s duration and the track’s duration is expressed as an implicit empty edit.
+									uint32_t segment_duration; //integer that specifies the duration of this edit segment in units of the movieÂ’s time scale.
+									uint32_t segment_mediaTime; //integer containing the starting time within the media of this edit segment(in media timescale units).If this field is set to ?, it is an empty edit.The last edit in a track should never be an empty edit.Any difference between the movieÂ’s duration and the trackÂ’s duration is expressed as an implicit empty edit.
 									uint32_t segment_mediaRate; //point number that specifies the relative rate at which to play the media corresponding to this edit segment.This rate value cannot be 0 or negative.
 									for (i = 0; i < readnum; i++)
 									{
